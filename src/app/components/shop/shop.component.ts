ad0378c6890b5106ba8a0e733eae9193a9d6746d
Juan Tenorio Arzola <16966425+tenoriojuann@users.noreply.github.com>
2021-08-20 23:14:05 -0400

    [TCS-1] Adds more content for Adsense

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
+  welcomeMessage = 'Hi,\n Thank you for checking us out!\nYou will find some of our items below.\n Find us on Etsy or Facebook Marketplace!'
+
+  constructor(
+    private renderer2: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit(): void {
+    this.document.getElementById('etsy-page').after(this.document.getElementById('etsy'))
   }
+
 
 }
